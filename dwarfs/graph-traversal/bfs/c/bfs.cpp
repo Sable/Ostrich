f9038e951d989881528b3c939c17f963a2d976ce
Vincent Foley <vfoley@gmail.com>
2014-04-30 10:51:20 -0400

    Updated bfs benchmark
    - Used common_rand() function for initialization
    - Used stopwatch to report init and traversal times

--- dwarfs/graph-traversal/bfs/c/bfs.cpp
+++ dwarfs/graph-traversal/bfs/c/bfs.cpp
@@ -2,36 +2,50 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-#include <omp.h>
-//#define NUM_THREAD 4
-#define OPEN
+#include <limits.h>
+#include <vector>
 
-int no_of_nodes;
-int edge_list_size;
-FILE *fp;
+#include "../common/common_rand.h"
+#include "../common/common.h"
+
+
+#define MIN_NODES      20
+#define MAX_NODES      ULONG_MAX
+#define MIN_EDGES      2
+#define MAX_INIT_EDGES 4 // Nodes will have, on average, 2*MAX_INIT_EDGES edges
+#define MIN_WEIGHT     1
+#define MAX_WEIGHT     10
+
+
+using namespace std;
 
 //Structure to hold a node information
 struct Node
 {
-	int starting;
-	int no_of_edges;
+    int starting;
+    int no_of_edges;
+};
+
+
+struct edge; // forward declaration
+typedef vector<edge> node;
+struct edge {
+    unsigned long dest;
+    unsigned weight;
 };
 
 void BFSGraph(int argc, char** argv);
+void InitializeGraph(Node**, bool**, bool**, bool**, int**, int**, int);
 
-void Usage(int argc, char**argv){
-
-fprintf(stderr,"Usage: %s <num_threads> <input_file>\n", argv[0]);
-
+void Usage(char**argv) {
+    fprintf(stderr,"Usage: %s <num_nodes>\n", argv[0]);
 }
 ////////////////////////////////////////////////////////////////////////////////
 // Main Program
 ////////////////////////////////////////////////////////////////////////////////
-int main( int argc, char** argv)
-{
-	no_of_nodes=0;
-	edge_list_size=0;
-	BFSGraph( argc, argv);
+int main( int argc, char** argv) {
+    BFSGraph( argc, argv );
+    return 0;
 }
 
 
@@ -39,136 +53,158 @@ int main( int argc, char** argv)
 ////////////////////////////////////////////////////////////////////////////////
 //Apply BFS on a Graph using CUDA
 ////////////////////////////////////////////////////////////////////////////////
-void BFSGraph( int argc, char** argv)
-{
-    char *input_f;
-	int	 num_omp_threads;
+void BFSGraph( int argc, char** argv) {
+    if (argc != 2) {
+        Usage(argv);
+        exit(0);
+    }
 
-	if(argc!=3){
-	Usage(argc, argv);
-	exit(0);
-	}
+    int no_of_nodes = atoi(argv[1]);
 
-	num_omp_threads = atoi(argv[1]);
-	input_f = argv[2];
+    Node *h_graph_nodes;
+    bool *h_graph_mask;
+    bool *h_updating_graph_mask;
+    bool *h_graph_visited;
+    int  *h_graph_edges;
+    int  *h_cost;
+    stopwatch sw1, sw2;
 
-	printf("Reading File\n");
-	//Read in Graph from a file
-	fp = fopen(input_f,"r");
-	if(!fp)
-	{
-		printf("Error Reading graph file\n");
-		return;
-	}
+    stopwatch_start(&sw2);
+    InitializeGraph(
+        &h_graph_nodes,
+        &h_graph_mask,
+        &h_updating_graph_mask,
+        &h_graph_visited,
+        &h_graph_edges,
+        &h_cost,
+        no_of_nodes);
+    stopwatch_stop(&sw2);
 
-	int source = 0;
-
-	fscanf(fp,"%d",&no_of_nodes);
-
-	// allocate host memory
-	Node* h_graph_nodes = (Node*) malloc(sizeof(Node)*no_of_nodes);
-	bool *h_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
-	bool *h_updating_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
-	bool *h_graph_visited = (bool*) malloc(sizeof(bool)*no_of_nodes);
-
-	int start, edgeno;
-	// initalize the memory
-	for( unsigned int i = 0; i < no_of_nodes; i++)
-	{
-		fscanf(fp,"%d %d",&start,&edgeno);
-		h_graph_nodes[i].starting = start;
-		h_graph_nodes[i].no_of_edges = edgeno;
-		h_graph_mask[i]=false;
-		h_updating_graph_mask[i]=false;
-		h_graph_visited[i]=false;
-	}
-
-	//read the source node from the file
-	fscanf(fp,"%d",&source);
-	source=0;
-
-	//set the source node as true in the mask
-	h_graph_mask[source]=true;
-	h_graph_visited[source]=true;
-
-	fscanf(fp,"%d",&edge_list_size);
-
-	int id,cost;
-	int* h_graph_edges = (int*) malloc(sizeof(int)*edge_list_size);
-	for(int i=0; i < edge_list_size ; i++)
-	{
-		fscanf(fp,"%d",&id);
-		fscanf(fp,"%d",&cost);
-		h_graph_edges[i] = id;
-	}
-
-	if(fp)
-		fclose(fp);
+    int k=0;
 
 
-	// allocate mem for the result on host side
-	int* h_cost = (int*) malloc( sizeof(int)*no_of_nodes);
-	for(int i=0;i<no_of_nodes;i++)
-		h_cost[i]=-1;
-	h_cost[source]=0;
 
-	printf("Start traversing the tree\n");
+    stopwatch_start(&sw1);
+    bool stop;
+    do
+    {
+//if no thread changes this value then the loop stops
+        stop=false;
 
-	int k=0;
+        for(int tid = 0; tid < no_of_nodes; tid++ )
+        {
+            if (h_graph_mask[tid] == true){
+                h_graph_mask[tid]=false;
+                for(int i=h_graph_nodes[tid].starting; i<(h_graph_nodes[tid].no_of_edges + h_graph_nodes[tid].starting); i++)
+                {
+                    int id = h_graph_edges[i];
+                    if(!h_graph_visited[id])
+                    {
+                        h_cost[id]=h_cost[tid]+1;
+                        h_updating_graph_mask[id]=true;
+                    }
+                }
+            }
+        }
 
-	bool stop;
-	do
-	{
-		//if no thread changes this value then the loop stops
-		stop=false;
+        for(int tid=0; tid< no_of_nodes ; tid++ )
+        {
+            if (h_updating_graph_mask[tid] == true){
+                h_graph_mask[tid]=true;
+                h_graph_visited[tid]=true;
+                stop=true;
+                h_updating_graph_mask[tid]=false;
+            }
+        }
+        k++;
+    }
+    while(stop);
+    stopwatch_stop(&sw1);
 
-#ifdef OPEN
-		omp_set_num_threads(num_omp_threads);
-		#pragma omp parallel for
-#endif
-		for(int tid = 0; tid < no_of_nodes; tid++ )
-		{
-			if (h_graph_mask[tid] == true){
-			h_graph_mask[tid]=false;
-			for(int i=h_graph_nodes[tid].starting; i<(h_graph_nodes[tid].no_of_edges + h_graph_nodes[tid].starting); i++)
-				{
-				int id = h_graph_edges[i];
-				if(!h_graph_visited[id])
-					{
-					h_cost[id]=h_cost[tid]+1;
-					h_updating_graph_mask[id]=true;
-					}
-				}
-			}
-		}
+    fprintf(stderr, "Init time     : %fs\n", get_interval_by_sec(&sw2));
+    fprintf(stderr, "Traversal time: %fs\n", get_interval_by_sec(&sw1));
 
-  		for(int tid=0; tid< no_of_nodes ; tid++ )
-		{
-			if (h_updating_graph_mask[tid] == true){
-			h_graph_mask[tid]=true;
-			h_graph_visited[tid]=true;
-			stop=true;
-			h_updating_graph_mask[tid]=false;
-			}
-		}
-		k++;
-	}
-	while(stop);
-
-	//Store the result into a file
-	FILE *fpo = fopen("result.txt","w");
-	for(int i=0;i<no_of_nodes;i++)
-		fprintf(fpo,"%d) cost:%d\n",i,h_cost[i]);
-	fclose(fpo);
-	printf("Result stored in result.txt\n");
+    for(int i=0;i<no_of_nodes;i++)
+        printf("%d) cost:%d\n",i,h_cost[i]);
 
 
-	// cleanup memory
-	free( h_graph_nodes);
-	free( h_graph_edges);
-	free( h_graph_mask);
-	free( h_updating_graph_mask);
-	free( h_graph_visited);
-	free( h_cost);
+    free(h_graph_nodes);
+    free(h_graph_edges);
+    free(h_graph_mask);
+    free(h_updating_graph_mask);
+    free(h_graph_visited);
+    free(h_cost);
+
+}
+
+
+
+void InitializeGraph(
+    Node **h_graph_nodes,
+    bool **h_graph_mask,
+    bool **h_updating_graph_mask,
+    bool **h_graph_visited,
+    int  **h_graph_edges,
+    int  **h_cost,
+    int numNodes) {
+
+    node *graph = new node[numNodes];
+    int source = 0;
+    unsigned numEdges;
+    unsigned long nodeID;
+    unsigned weight;
+
+    *h_graph_nodes = (Node*) malloc(sizeof(Node)*numNodes);
+    *h_graph_mask = (bool*) malloc(sizeof(bool)*numNodes);
+    *h_updating_graph_mask = (bool*) malloc(sizeof(bool)*numNodes);
+    *h_graph_visited = (bool*) malloc(sizeof(bool)*numNodes);
+    *h_cost = (int*) malloc(sizeof(int)*numNodes);
+
+    for (int i = 0; i < numNodes; ++i) {
+        numEdges = abs(common_rand() % ( MAX_INIT_EDGES - MIN_EDGES + 1 )) + MIN_EDGES;
+        for ( unsigned j = 0; j < numEdges; j++ ) {
+            nodeID = abs(common_rand() % numNodes);
+            weight = abs(common_rand() % ( MAX_WEIGHT - MIN_WEIGHT + 1 )) + MIN_WEIGHT;
+            graph[i].push_back( edge() );
+            graph[i].back().dest = nodeID;
+            graph[i].back().weight = weight;
+            graph[nodeID].push_back( edge() );
+            graph[nodeID].back().dest = i;
+            graph[nodeID].back().weight = weight;
+        }
+    }
+
+    unsigned long totalEdges = 0;
+    for (int i = 0; i < numNodes; ++i) {
+        unsigned long numEdges = graph[i].size();
+        (*h_graph_nodes)[i].starting = totalEdges;
+        (*h_graph_nodes)[i].no_of_edges = numEdges;
+        (*h_graph_mask)[i] = false;
+        (*h_updating_graph_mask)[i] = false;
+        (*h_graph_visited)[i] = false;
+
+        totalEdges += numEdges;
+    }
+
+    *h_graph_edges = (int*) malloc(sizeof(int)*totalEdges);
+
+
+    (*h_graph_mask)[source] = true;
+    (*h_graph_visited)[source] = true;
+
+    unsigned k = 0;
+    for ( unsigned long i = 0; i < numNodes; i++ ) {
+        for ( unsigned j = 0; j < graph[i].size(); j++ ) {
+            (*h_graph_edges)[k] = graph[i][j].dest;
+            ++k;
+        }
+    }
+
+    for(int i = 0; i < numNodes; ++i) {
+        (*h_cost)[i] = -1;
+    }
+    (*h_cost)[source] = 0;
+
+    delete[] graph;
 
 }
