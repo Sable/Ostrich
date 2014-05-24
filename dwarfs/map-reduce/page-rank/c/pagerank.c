65b9c0a0d558302b837473cba9b5e216952e11bb
Erick Lavoie <github@ericklavoie.com>
2014-05-23 23:04:20 -0400

    PAGERANK: Fixed C and JS versions, no self-checking yet

--- dwarfs/map-reduce/page-rank/c/pagerank.c
+++ dwarfs/map-reduce/page-rank/c/pagerank.c
@@ -1,3 +1,4 @@
+// Taken from: http://wwwhome.ewi.utwente.nl/~fokkinga/mmf2010e.pdf
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,14 +15,19 @@ const float threshold= 0.00001;
 
 
 // generates an array of random pages and their links
-int *random_pages(int n, unsigned int *noutlinks){
+int *random_pages(int n, unsigned int *noutlinks, int divisor){
     int i, j, k;
     int *pages = malloc(sizeof(*pages)*n*n); // matrix 1 means link from j->i
+    
+    if (divisor <= 0) {
+        printf("ERROR: Invalid divisor '%d' for random initialization, divisor should be greater or equal to 1\n", divisor);
+        exit(1);
+    }
 
     for(i=0; i<n; ++i){
         noutlinks[i] = 0;
         for(j=0; j<n; ++j){
-            if(i!=j && (common_rand()%2 == 1)){
+            if(i!=j && (common_rand()%divisor == 0)){
                 pages[i*n+j] = 1;
                 noutlinks[i] += 1;
             }
@@ -46,34 +52,32 @@ void init_array(float *a, int n, float val){
 
 void map_page_rank(int *pages, float *page_ranks, float *maps, unsigned int *noutlinks, int n){
     int i,j;
+    float outbound_rank = 0.0;
     for(i=0; i<n; ++i){
+        outbound_rank = page_ranks[i]/(float)noutlinks[i];
         for(j=0; j<n; ++j){
-            if(pages[i*n+j] == 1){
-                maps[i*n+j] = page_ranks[j]/(float)noutlinks[j];
-            }
-            else{
-                maps[i*n+j] = 0.0f;
-            }
+            maps[i*n+j] = pages[i*n+j] == 0 ? 0.0f : pages[i*n+j]*outbound_rank;
         }
     }
 }
 
-void reduce_page_rank(float *page_ranks, float *maps, int n, float *max_diff){
+float reduce_page_rank(float *page_ranks, float *maps, int n){
     int i, j;
     float dif = 0.0f;
     float new_rank;
+    float old_rank;
 
-    for(i=0; i< n; ++i){
+    for(j=0; j<n; ++j){
+        old_rank = page_ranks[j];
         new_rank = 0.0f;
-        for(j=0; j<n; ++j){
+        for(i=0; i< n; ++i){
             new_rank += maps[i*n + j];
         }
-
         new_rank = ((1-d_factor)/n)+(d_factor*new_rank);
-        page_ranks[i] = new_rank;
-        dif = fabs(new_rank - page_ranks[i]) > dif ? fabs(new_rank - page_ranks[i]) : dif;
+        dif = fabs(new_rank - old_rank) > dif ? fabs(new_rank - old_rank) : dif;
+        page_ranks[j] = new_rank;
     }
-    *max_diff = dif;
+    return dif;
 }
 
 void usage(char *argv[]){
@@ -96,15 +100,19 @@ int main(int argc, char *argv[]){
     int t;
     float max_diff;
     stopwatch sw;
-
+    
+    int i = 0;
+    int j;
     int n = 1000;
+    float sum = 0;
     int iter = max_iter;
     float thresh = threshold;
+    int divisor = 2;
+    int nb_links = 0;
 
     int opt, opt_index = 0;
-    while((opt = getopt_long(argc, argv, "::n:i:t:", size_opts, &opt_index)) != -1)
+    while((opt = getopt_long(argc, argv, "::n:i:t:d:", size_opts, &opt_index)) != -1)
     {
-        //printf("here");
         switch(opt)
         {
         case 'n':
@@ -116,24 +124,46 @@ int main(int argc, char *argv[]){
         case 't':
             thresh = atof(optarg);
             break;
+        case 'd':
+            divisor = atoi(optarg);
+            break;
         default:
             usage(argv);
             exit(EXIT_FAILURE);
         }
     }
     max_diff=99.0f;
-    page_ranks = malloc(sizeof(*page_ranks)*n);
-    maps = malloc(sizeof(*maps)*n*n);
-    noutlinks = malloc(sizeof(*noutlinks)*n);
+    page_ranks = (float*)malloc(sizeof(*page_ranks)*n);
+    maps = (float*)malloc(sizeof(*maps)*n*n);
+    noutlinks = (unsigned int*)malloc(sizeof(*noutlinks)*n);
 
-    memset(noutlinks, 0, sizeof(*noutlinks)*n);
-    pages = random_pages(n,noutlinks);
+    for (i=0; i<n; ++i) {
+        noutlinks[i] = 0;
+    }
+    pages = random_pages(n,noutlinks,divisor);
     init_array(page_ranks, n, 1.0f / (float) n);
 
+    nb_links = 0;
+    for (i=0; i<n; ++i) {
+        for (j=0; j<n; ++j) {
+            nb_links += pages[i*n+j];
+        }
+    }
+    //printf("nb of links: %d/%d\n",nb_links,n*n);
+
     stopwatch_start(&sw);
-    for(t=0; t< iter; ++t){
+
+    for(t=1; t<=iter && max_diff>=thresh; ++t){
         map_page_rank(pages, page_ranks, maps, noutlinks, n);
-        reduce_page_rank(page_ranks, maps, n, &max_diff);
+        max_diff = reduce_page_rank(page_ranks, maps, n);
+
+        printf("PageRanks(%.3d) [diff=%.10f]: ", t, max_diff);
+        sum = 0;
+        for (i=0; i<n; ++i) {
+            //printf("%.4f ", page_ranks[i]); 
+            sum += page_ranks[i];
+        }
+        printf(" sum=%.3f \n",sum);
     }
     stopwatch_stop(&sw);
 
