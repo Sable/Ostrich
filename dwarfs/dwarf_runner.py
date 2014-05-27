0ac456ed91d1f5f3be58e8aec192b4f41c085aaa
Vincent Foley <vfoley@gmail.com>
2014-05-27 10:01:39 -0400

    Added initial version of webbench.py

--- dwarfs/dwarf_runner.py
+++ dwarfs/dwarf_runner.py
@@ -3,6 +3,21 @@
 import subprocess
 import json
 import os
+import threading
+import time
+
+class WebbenchThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.out = "[]"
+
+    def run(self):
+        webserver_script = ["python", "webbench.py"]
+        stdout, stderr = subprocess.Popen(webserver_script,
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE).communicate()
+        self.out = stdout[:]
+
 
 class Benchmark(object):
     def __init__(self, name, dir):
@@ -28,6 +43,23 @@ class Benchmark(object):
         return [r['time'] for r in results]
 
 
+    def run_asmjs_benchmark(self, browser, browser_opts):
+        """Run the asm.js inside the browser with the specified opts."""
+        webserver_script = ["python", "webbench.py"]
+        browser_script = [browser] + browser_opts + \
+            ["http://0.0.0.0:8080/static/" + os.path.join(self.dir, "build", "asmjs", "run.html")]
+
+        # Start webserver
+        thr = WebbenchThread()
+        thr.start()
+
+        # Start browser
+        time.sleep(1)
+        subprocess.call(browser_script, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        thr.join()
+        return json.loads(thr.out)
+
+
     def build(self):
         """Move into the benchmark's directory and run make clean && make."""
 
@@ -48,7 +80,7 @@ BENCHMARKS = [
     Benchmark("bfs", "graph-traversal/bfs"),
     Benchmark("page-rank", "map-reduce/page-rank"),
     Benchmark("lavamd", "n-body-methods/lavamd"),
-    Benchmark("spmv", "sparse-linear-algebra/spmv"),
+    #Benchmark("spmv", "sparse-linear-algebra/spmv"),
     Benchmark("fft", "spectral-methods/fft"),
     Benchmark("srad", "structured-grid/SRAD"),
     Benchmark("back-propagation", "unstructured-grid/back-propagation"),
@@ -58,4 +90,6 @@ for b in BENCHMARKS:
     b.build()
 
 for b in BENCHMARKS:
-    print "%s,%s" % (b.name, ','.join(str(x) for x in b.run_c_benchmark()))
+    print "%s,C,N/A,%s" % (b.name, ','.join(str(x) for x in b.run_c_benchmark()))
+    print "%s,asmjs,Chrome,%s" % (b.name, ','.join(str(x) for x in b.run_asmjs_benchmark("google-chrome", [])))
+    print "%s,asmjs,Firefox,%s" % (b.name, ','.join(str(x) for x in b.run_asmjs_benchmark("firefox", [])))
