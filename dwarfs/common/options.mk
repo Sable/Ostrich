8ad8cd88a22f5056edaeb42992a3a18a0f61926b
Vincent Foley <vfoley@gmail.com>
2014-06-03 11:17:39 -0400

    Modified the Makefiles to take opencl platform and device information from variables.

--- dwarfs/common/options.mk
+++ dwarfs/common/options.mk
@@ -7,3 +7,6 @@ EMCPP = em++ -O2
 OPENCL_DIR = /usr/lib64/OpenCL/vendors/intel/
 OPENCL_INC = $(OPENCL_DIR)/include/
 OPENCL_LIB = $(OPENCL_DIR)/lib/x86_64/ -lOpenCL
+
+OPENCL_PLATFORM = 0
+OPENCL_DEVICE   = 0
